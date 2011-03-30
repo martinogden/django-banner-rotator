5a4ecf6d0c1cbca8ea0f0fb65360436d0f6cfe35
Martin Ogden <martin@cahoona.co.uk>
2011-03-30 18:08:07 +0100

    Add click count field to banner inline admin

--- banner_rotator/admin.py
+++ banner_rotator/admin.py
@@ -1,12 +1,40 @@
+import logging
+
 from django.contrib import admin
+from django import forms
 
 from banner_rotator.models import Campaign, Banner
+
+class BannerForm(forms.ModelForm):
+    """
+    We override the form to add the click count as a 'hidden' field,
+        Field is a textinput, and looks invisible due to css styling.
+
+    This is a little hacky but it's an easy workaround, and isn't public facing
+    """
+    clicks = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={'readonly':'readonly', 'style':'border: none; padding: 0;'}))
+
+    class Meta:
+        model = Banner
+
+    def __init__(self, *args, **kwargs):
+        super(BannerForm, self).__init__(*args, **kwargs)
+
+        # Set the form fields based on the model object
+        if kwargs.has_key('instance'):
+            instance = kwargs['instance']
+            self.initial['clicks'] = instance.clicks.count()
+        else:
+            self.initial['clicks'] = 0
 
 
 class BannerAdminInline(admin.StackedInline):
     model = Banner
-    extra = 1
-    readonly_fields = ['impressions', 'clicks']
+    form = BannerForm
+    extra = 0
+    readonly_fields = ['impressions',]
+    fields = ['is_active', 'name', 'url', 'image', 'clicks', 'impressions']
 
 
 class CampaignAdmin(admin.ModelAdmin):
