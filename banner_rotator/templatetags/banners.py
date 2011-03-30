38a8508bf5dc507ddb936fc3be68db8a3732abab
Martin Ogden <martin@cahoona.co.uk>
2011-03-30 18:09:26 +0100

    Add templatetags to display banners

---
+++ banner_rotator/templatetags/banners.py
@@ -0,0 +1,42 @@
+from django import template
+
+from banner_rotator.models import Banner
+
+register = template.Library()
+
+
+class BannerNode(template.Node):
+
+    def __init__(self, varname='banner', campaign_id=None):
+        self.varname, self.campaign_id = varname, campaign_id
+
+    def render(self, context):
+        kwargs = {}
+        if self.campaign_id:
+            kwargs['campaign'] = self.campaign_id
+        banner = Banner.objects.biased_choice(**kwargs)
+        banner.view()
+        context[self.varname] = banner
+        return ''
+
+
+@register.tag
+def banner(parser, token):
+    """
+    Use: {% banner 1 as banner %}
+
+    Pick a banner using the biased / weighting manager, with an optional campaign number
+    """
+    bits = token.contents.split()
+
+    if len(bits) not in [3,4]:
+        raise template.TemplateSyntaxError, "pick_banner tag takes three of four arguments"
+    
+    if bits[2] == 'as':
+        varname = bits[3]
+        campaign = int(bits[1])
+    else:
+        varname = bits[2]
+        campaign = None
+
+    return BannerNode(varname, campaign)
