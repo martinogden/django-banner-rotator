4e9032cf8cdf39c9b0ccfc274c9573c4839f260a
Aleksandr Zorin <plazix@gmail.com>
2011-12-15 23:17:15 +0400

    todo написать тесты

--- banner_rotator/tests.py
+++ banner_rotator/tests.py
@@ -1,15 +1,5 @@
+#-*- coding:utf-8 -*-
+
 from django.test import TestCase
-from banner_rotator.models import Banner
 
-class AdtoTest(TestCase):
-    """
-    Make sure management commands updates the database
-    """
-    fixtures = ['banner_rotator.json']
-
-    def testBiasedChoice(self):
-        """
-        We can't test for randomness, however we can test
-            for the function returning a single element
-        """
-        self.assertEqual(1, len([Banner.objects.biased_choice()]))
+#todo написать тесты
