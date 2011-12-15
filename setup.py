4d9a599fe1bdd03bbcba87980f4bda85197ea669
Oleg Bondarenko <millioner.bbb@gmail.com>
2011-12-15 20:00:57 +0200

    And one more setup fix :)

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='django-banner-rotator',
-    version='0.1.1',
+    version='0.1',
     description='Simple Banner Rotation App for Django',
     author='Martin Ogden',
     url='http://github.com/martinogden/django-banner-rotator',
     include_package_data = True,
-    packages=['banner_rotator', ],
+    packages=find_packages(),
 )
 
