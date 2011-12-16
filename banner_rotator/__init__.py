0167ce3d89ffe891a946285dcb213bdec9b08b4d
Aleksandr Zorin <plazix@gmail.com>
2011-12-16 11:30:53 +0400

    обновлен README, увеличен номер версии

--- banner_rotator/__init__.py
+++ banner_rotator/__init__.py
@@ -1,1 +1,19 @@
 #-*- coding:utf-8 -*-
+
+__all__ = ['get_version']
+
+VERSION = (0, 2, 0, 'beta', 1)
+
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            version = "%s %s" % (version, VERSION[3])
+            if VERSION[4] != 0:
+                version = '%s %s' % (version, VERSION[4])
+    return version
--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
+from banner_rotator import get_version
+
+
 setup(name='django-banner-rotator',
-    version='0.1',
-    description='Simple Banner Rotation App for Django',
+    version=get_version().replace(' ', '-'),
+    description='Banner Rotation App for Django',
     author='Martin Ogden',
     url='http://github.com/martinogden/django-banner-rotator',
     include_package_data = True,
+    zip_safe=False,
     packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
 )
 
