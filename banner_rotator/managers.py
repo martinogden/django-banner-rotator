d5e3a264403fa6c536aaa6833eb6fd5c54d7cc5e
Martin Ogden <martino@potatolondon.com>
2012-11-21 16:02:41 +0000

    Fix AssertionError bug in pick method
     * Fix bug where %f string interpolation was causing numbers to be rounded
    
     * Catch AssertionError in pick method and normalize weights
    
     * Add basic test (will add more later)

--- banner_rotator/managers.py
+++ banner_rotator/managers.py
@@ -1,43 +1,41 @@
 #-*- coding:utf-8 -*-
 
 from random import random
-from decimal import Decimal
 
 from django.db import models
 
 
 def pick(bias_list):
+    """ Takes a list of 2-tuples [(item, weight)] using weight as the
+        probability when calculating an item to choose
     """
-    Takes a list similar to [(item1, item1_weight), (item2, item2_weight),]
-        and item(n)_weight as the probability when calculating an item to choose
-    """
+    try:
+        # First, normalize weights to ensure we
+        # have a valid probability distribution
+        assert sum([w for i, w in bias_list]) == 1
+    except AssertionError:
+        norm_const = float(sum([w for i, w in bias_list]))
+        distribution = [(i, w / norm_const) for i, w in bias_list]
+    else:
+        distribution = bias_list
 
-    # All weights should add up to 1
-    #   an items weight is equivalent to it's probability of being picked
-    assert sum(i[1] for i in bias_list) == 1
+    number = random()
+    current = 0.0
 
-    # Django ORM returns floats as Decimals,
-    #   so we'll convert floats to decimals here to co-operate
-    number = Decimal("%.18f" % random())
-    current = Decimal(0)
-
-    # With help from
-    #   @link http://fr.w3support.net/index.php?db=so&id=479236
-    for choice, bias in bias_list:
-        current += bias
+    # @link http://fr.w3support.net/index.php?db=so&id=479236
+    for choice, probability in distribution:
+        current += probability
         if number <= current:
             return choice
 
 
 class BannerManager(models.Manager):
+
     def biased_choice(self, place):
         queryset = self.filter(is_active=True, places=place)
 
         if not queryset.count():
             raise self.model.DoesNotExist
 
-        calculations = queryset.aggregate(weight_sum=models.Sum('weight'))
-
-        banners = queryset.extra(select={'bias': 'weight/%f' % calculations['weight_sum']})
-
-        return pick([(b, b.bias) for b in banners])
+        normalizer = queryset.aggregate(normalizer=models.Sum('weight'))['normalizer']
+        return pick([(i, i.weight / float(normalizer)) for i in queryset])
--- banner_rotator/tests.py
+++ banner_rotator/tests.py
@@ -1,5 +1,33 @@
-#-*- coding:utf-8 -*-
-
 from django.test import TestCase
+from .managers import pick
 
-#todo написать тесты
+
+class BaseBannerTest(TestCase):
+    fixtures = ['test_data']
+
+
+class BannerManagerTest(BaseBannerTest):
+
+    def test_pick(self):
+        # check with a item of probability 1
+        choices = [
+            (True, 1),
+            (False, 0),
+            (False, 0),
+            (False, 0),
+            (False, 0),
+        ]
+        result = pick(choices)
+        self.assertTrue(result)
+
+        # Ensure nothing funky happens if we have an
+        # invalid probability distribution
+        choices = [
+            (True, 1),
+            (True, 1),
+            (False, 0),
+            (False, 0),
+            (False, 0),
+        ]
+        result = pick(choices)
+        self.assertTrue(result)
